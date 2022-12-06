56eec8dc9bc9d8482a5fda1f3c2e886dcad7c5b6
meyercode <adam@meyerfam.com>
2022-12-06 06:39:08 +0100

    day4, pt 1

---
+++ 2022/day4/main.py
@@ -0,0 +1,28 @@
+f = open("./input","r")
+lines = f.readlines()
+
+intersectCount = 0
+
+for line in lines:
+    line = line.strip("\\n")
+    pair = line.split(",")
+    rangeA = pair[0].split("-")
+    rangeB = pair[1].split("-")
+
+    elfA = []
+    elfB = []
+    for section in range(int(rangeA[0]), int(rangeA[1])+1):
+        elfA.append(section)
+    for section in range(int(rangeB[0]), int(rangeB[1])+1):
+        elfB.append(section)
+
+    aMinSect = elfA[0]
+    aMaxSect = elfA[len(elfA)-1]
+    
+    bMinSect = elfB[0]
+    bMaxSect = elfB[len(elfB)-1]
+
+    if aMinSect <= bMinSect and aMaxSect >= bMaxSect or bMinSect <= aMinSect and bMaxSect >= aMaxSect:
+        intersectCount += 1
+
+print(intersectCount)
